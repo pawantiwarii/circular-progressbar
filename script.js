664c3ae014d9c1a4c1c99ddc503d172d00e3fa7c
PAWANTIWARII <pt7697479@gmail.com>
2024-09-21 01:14:13 +0530

    circular progress bar

---
+++ script.js
@@ -0,0 +1,13 @@
+const number = document.querySelector("#number");
+
+let counter = 0;
+
+setInterval(() => {
+    if(counter == 65){
+        clearInterval()
+    }else{
+
+        counter++
+        number.innerHTML = counter + "%"
+    }
+}, 28);
